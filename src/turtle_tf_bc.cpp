0fe7b626b79eb6ae8e6e995487c7a0439d9d7644
furrypiglet <1125411619@qq.com>
2024-04-16 16:19:50 +0800

    代码格式化

--- src/turtle_tf_bc.cpp
+++ src/turtle_tf_bc.cpp
@@ -4,17 +4,17 @@
 
 std::string turtle_name;
 
-void poseCallback(const turtlesim::PoseConstPtr& msg)
+void poseCallback(const turtlesim::PoseConstPtr &msg)
 {
 	// 创建tf的广播器
 	static tf::TransformBroadcaster br;
 
 	// 初始化tf数据
 	tf::Transform transform;
-    //turtle1的xy坐标作为原点
-	transform.setOrigin( tf::Vector3(msg->x, msg->y, 0.0) );
+	// turtle1的xy坐标作为原点
+	transform.setOrigin(tf::Vector3(msg->x, msg->y, 0.0));
 	tf::Quaternion q;
-    //旋转轴设为Y轴
+	// 旋转轴设为Y轴
 	q.setRPY(0, 0, msg->theta);
 	transform.setRotation(q);
 
@@ -22,15 +22,15 @@ void poseCallback(const turtlesim::PoseConstPtr& msg)
 	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", turtle_name));
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    // 初始化ROS节点
+	// 初始化ROS节点
 	ros::init(argc, argv, "my_tf_broadcaster");
 
-	// 要求有2个刚体，参数不为2则退出
+	// 要求有2个海龟，参数不为2则退出
 	if (argc != 2)
 	{
-		ROS_ERROR("need turtle name as argument"); 
+		ROS_ERROR("need turtle name as argument");
 		return -1;
 	}
 
@@ -38,9 +38,9 @@ int main(int argc, char** argv)
 
 	// 订阅海龟的位姿话题
 	ros::NodeHandle node;
-	ros::Subscriber sub = node.subscribe(turtle_name+"/pose", 10, &poseCallback);
+	ros::Subscriber sub = node.subscribe(turtle_name + "/pose", 10, &poseCallback);
 
-    // 循环等待回调函数
+	// 循环等待回调函数
 	ros::spin();
 
 	return 0;
